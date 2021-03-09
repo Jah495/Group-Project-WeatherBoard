d7f0a6a7efdb9f3ce66d2610c1640a4d760d3307
John Hernandez <jaherna432@gmail.com>
2021-03-08 23:53:08 -0600

    starting code

---
+++ Assets/javascript/script.js
@@ -0,0 +1,9 @@
+var city = document.querySelector('#navbar-brand');
+var submit = document.querySelector('submit');
+var apiUrl = 'https://openweathermap.org/api'
+var cityUrl = 'api.openweathermap.org/data/2.5/weather?q={city name}&appid=5503c4b40d8619632444aceb51771285'
+
+var citySelector = function (submit){
+'api.openweathermap.org/data/2.5/weather?q=', city, '&appid=5503c4b40d8619632444aceb51771285'
+}
+
